da3533fa59dc2c708bcddcf76d9282b75a809062
Rúnar Vestmann <runarvestmann@gmail.com>
2021-08-09 01:14:33 +0000

    Added email service

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import { createTheme, ThemeProvider } from '@material-ui/core';
 
 const App = () => {
     return (
-        <Router>
-            <NavigationBar />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/projects" component={Projects} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/contact" component={Contact} />
-                <Route path="*" render={() => <Redirect to="/" />} />
-            </Switch>
-        </Router>
+        <ThemeProvider theme={createTheme({ palette: { type: 'dark' } })}>
+            <Router>
+                <NavigationBar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/projects" component={Projects} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route path="*" render={() => <Redirect to="/" />} />
+                </Switch>
+            </Router>
+        </ThemeProvider>
     );
 };
 
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,18 +1,87 @@
+import { TextField, Button, CircularProgress, Typography } from '@material-ui/core';
+import { useState } from 'react';
 import './style.css';
 
 const Contact = () => {
+    const [state, setState] = useState({ subject: '', message: '' });
+    const [messageStatus, setMessageStatus] = useState({ messageSent: false, sendingMessage: false });
+    const handleChange = (ev) => setState((state) => ({ ...state, [ev.target.name]: ev.target.value }));
+    const [errors, setErrors] = useState({});
+    const handleSubmit = () => {
+        const errors = {};
+        if (state.subject.length === 0) errors.subject = 'Subject is required';
+        if (state.message.length === 0) errors.message = 'Message is required';
+        if (Object.keys(errors).length > 0) return setErrors(errors);
+        setMessageStatus({ sendingMessage: true, messageSent: false });
+        sendMessage();
+    };
+
+    const sendMessage = async () => {
+        await fetch('https://contact-runarvestmann.herokuapp.com/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(state),
+        });
+        setMessageStatus({ messageSent: true, sendingMessage: false });
+    };
+
     return (
         <div className="section">
             <div id="contact">
-                <p>
-                    Feel free to send me an e-mail at{' '}
-                    <a
-                        className="emphasis"
-                        href="mai&#108;&#116;o&#58;r&#37;&#55;5narv&#101;&#37;73tma%6E%6E%4&#48;gmai%6&#67;&#46;co&#109;"
-                    >
-                        r&#117;na&#114;&#118;est&#109;&#97;nn&#64;gm&#97;il&#46;&#99;om
-                    </a>
-                </p>
+                {messageStatus.sendingMessage && <CircularProgress />}
+                {messageStatus.messageSent && <Typography variant="h4">Your message has been delivered</Typography>}
+                {!messageStatus.messageSent && !messageStatus.sendingMessage && (
+                    <>
+                        <TextField
+                            onChange={handleChange}
+                            name="subject"
+                            margin="dense"
+                            className="subject"
+                            size="medium"
+                            label="Subject"
+                            error={!!errors.subject}
+                            helperText={errors.subject}
+                            FormHelperTextProps={{
+                                style: {
+                                    fontSize: '0.8rem',
+                                },
+                            }}
+                        />
+                        <TextField
+                            onChange={handleChange}
+                            name="message"
+                            margin="dense"
+                            className="message"
+                            InputProps={{ style: { marginBottom: '2rem' } }}
+                            size="medium"
+                            label="Message"
+                            multiline
+                            error={!!errors.message}
+                            helperText={errors.message}
+                            FormHelperTextProps={{
+                                style: {
+                                    fontSize: '0.8rem',
+                                    marginTop: '-1.5rem',
+                                    marginBottom: '1rem',
+                                },
+                            }}
+                        />
+                        <Button
+                            size="small"
+                            style={{
+                                backgroundColor: '#b66a0c',
+                                fontWeight: 'bold',
+                            }}
+                            variant="contained"
+                            className="contact-btn"
+                            onClick={handleSubmit}
+                        >
+                            Send message
+                        </Button>
+                    </>
+                )}
             </div>
         </div>
     );
