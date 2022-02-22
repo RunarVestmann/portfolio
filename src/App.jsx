b0ee876b1268f7e87699d734184d746ecdcf2608
RunarVestmann <runarvestmann@gmail.com>
2022-02-22 20:58:35 +0000

    Downgraded the site due to dislike of layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import NavigationBar from './components/NavigationBar';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import { createTheme, ThemeProvider } from '@material-ui/core';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import { createTheme, ThemeProvider } from "@material-ui/core";
 
 const App = () => {
-    return (
-        <ThemeProvider theme={createTheme({ palette: { type: 'dark' } })}>
-            <Router>
-                <NavigationBar />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/contact" component={Contact} />
-                    <Route path="*" render={() => <Redirect to="/" />} />
-                </Switch>
-            </Router>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={createTheme({ palette: { type: "dark" } })}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="*" render={() => <Redirect to="/" />} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,23 +1,19 @@
-import { NavLink } from 'react-router-dom';
-import './style.css';
+import { NavLink } from "react-router-dom";
+import "./style.css";
 
 const NavigationBar = () => {
-    return (
-        <nav>
-            <NavLink activeClassName="selected-nav-link" className="nav-link" exact to="/">
-                Home
-            </NavLink>
-            <NavLink activeClassName="selected-nav-link" className="nav-link" exact to="/projects">
-                Projects
-            </NavLink>
-            <NavLink activeClassName="selected-nav-link" className="nav-link" exact to="/about">
-                About
-            </NavLink>
-            <NavLink activeClassName="selected-nav-link" className="nav-link" exact to="/contact">
-                Contact
-            </NavLink>
-        </nav>
-    );
+  return (
+    <nav>
+      <NavLink
+        activeClassName="selected-nav-link"
+        className="nav-link"
+        exact
+        to="/"
+      >
+        Home
+      </NavLink>
+    </nav>
+  );
 };
 
 export default NavigationBar;
