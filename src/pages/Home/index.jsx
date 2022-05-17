4a18696d0cd22217f727c2f218781304abb968c0
RunarVestmann <runarvestmann@gmail.com>
2022-05-17 21:10:02 +0000

    Changed bio

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,60 @@
-import { FaLinkedinIn, FaGithub, FaItchIo, FaTwitter, FaYoutube } from 'react-icons/fa';
-import './style.css';
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaItchIo,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import "./style.css";
 
 const Home = () => {
-    return (
-        <div className="section">
-            <div id="home">
-                <h1>
-                    Rúnar <span className="emphasis">Vestmann</span>
-                </h1>
-                <p>Software developer, CS student & Creator</p>
-                <div id="home-icons">
-                    <a href="https://is.linkedin.com/in/r%C3%BAnar-vestmann-a7229a19b" target="_blank" rel="noreferrer">
-                        <FaLinkedinIn className="icon" />
-                    </a>
-                    <a href="https://github.com/runarvestmann" target="_blank" rel="noreferrer">
-                        <FaGithub className="icon" />
-                    </a>
-                    <a href="https://runarvestmann.itch.io" target="_blank" rel="noreferrer">
-                        <FaItchIo className="icon" />
-                    </a>
-                    <a href="https://twitter.com/RunarVestmann" target="_blank" rel="noreferrer">
-                        <FaTwitter className="icon" />
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCzozM3fmOkAQND1OK70zRqA" target="_blank" rel="noreferrer">
-                        <FaYoutube className="icon" />
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className="section">
+      <div id="home">
+        <h1>
+          Rúnar <span className="emphasis">Vestmann</span>
+        </h1>
+        <p>Software developer, Teacher & Creator</p>
+        <div id="home-icons">
+          <a
+            href="https://is.linkedin.com/in/r%C3%BAnar-vestmann-a7229a19b"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedinIn className="icon" />
+          </a>
+          <a
+            href="https://github.com/runarvestmann"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub className="icon" />
+          </a>
+          <a
+            href="https://runarvestmann.itch.io"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaItchIo className="icon" />
+          </a>
+          <a
+            href="https://twitter.com/RunarVestmann"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitter className="icon" />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCzozM3fmOkAQND1OK70zRqA"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaYoutube className="icon" />
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
